0e305f1d1689b7d9da58a39264b7c2115dc88c60
Nady <mohamed.amr.nes@gmail.com>
2025-01-07 14:30:50 +0200

    patches: fs/devpts/inode.c

--- patches/patches.sh
+++ patches/patches.sh
@@ -12,7 +12,7 @@ patch_files=(
     fs/read_write.c
     fs/stat.c
     drivers/input/input.c
-    fs/internal.h
+    fs/devpts/inode.c
 )
 
 for i in "${patch_files[@]}"; do
@@ -83,6 +83,13 @@ for i in "${patch_files[@]}"; do
         sed -i '/static void input_handle_event/i\#ifdef CONFIG_KSU\nextern bool ksu_input_hook __read_mostly;\nextern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);\n#endif\n' drivers/input/input.c
         sed -i '/int disposition = input_get_disposition(dev, type, code, &value);/a\	#ifdef CONFIG_KSU\n	if (unlikely(ksu_input_hook))\n		ksu_handle_input_handle_event(&type, &code, &value);\n	#endif' drivers/input/input.c
         ;;
+
+    ## devpts/inode.c
+    fs/devpts/inode.c)
+        sed -i '/struct dentry \*devpts_pty_new/i\#ifdef CONFIG_KSU\nextern int ksu_handle_devpts(struct inode *);\n#endif' fs/devpts/inode.c
+        sed -i '/void \*devpts_get_priv(struct dentry \*dentry)/a\	#ifdef CONFIG_KSU\n	ksu_handle_devpts(dentry->d_inode);\n	#endif' fs/devpts/inode.c
+        ;;
+
     esac
 
 done
